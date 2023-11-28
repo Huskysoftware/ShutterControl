2b3b8cf09cff41c5769a55826f88ebc7f052b80d
bernd <bernd@huskysoftware.de>
2023-11-28 14:18:56 +0100

    Handle admin users differently

--- shutterControlTelegramBot.py
+++ shutterControlTelegramBot.py
@@ -17,10 +17,13 @@ _ = gettext.gettext
 
 INI_FILENAME: str = os.path.expanduser('~/.shutterControl.ini')
 INI_KEY_TELEGRAM_BOT_TOKEN: str = 'Telegram bot token'
+INI_KEY_ADMIN_USERS: str = 'Admins'
 INI_KEY_ALLOWED_TELEGRAM_USERS: str = 'Allowed Telegram usernames'
 
 UPDATE_EVERY_SECONDS: int = 10
 BOT_CONTACT_RETRY_DELAY_SECONDS: int = 5
+
+MSG_PARSE_MODE='MarkdownV2'
 
 CMDS_OFF = ('off', 'aus')
 CMDS_UP = ('up', 'rauf', 'auf', 'hoch', 'open')
@@ -74,7 +77,8 @@ HELP_TXT: str = _(
 
 
 bot: telepot.Bot = None
-allowed_chat_id: int = -1
+admins: tuple[str] = ()
+admin_chat_ids: set[int] = set()
 allowed_telegram_usernames: tuple[str] = ()
 next_event: shutterControl.Event = shutterControl.Event(False, None)
 
@@ -95,6 +99,10 @@ def modify_settings(
 	shutterControl.write_settings_to_db(
 		dawn_close, open_at, depression, latest)
 	update_next_event()
+
+
+def sendMsg(chat_id: int, msg: str):
+	bot.sendMessage(chat_id, msg, parse_mode=MSG_PARSE_MODE)
 
 
 def status_msg():
@@ -127,14 +135,29 @@ def status_msg():
 	return msg
 
 
-def send_status_msg():
-	if allowed_chat_id >= 0:
-		bot.sendMessage(allowed_chat_id, status_msg(), parse_mode='MarkdownV2')
+def send_status_msg(chat_id):
+	bot.sendMessage(chat_id, status_msg(), parse_mode=MSG_PARSE_MODE)
 
 
-def send_help_text():
-	if allowed_chat_id >= 0:
-		bot.sendMessage(allowed_chat_id, HELP_TXT, parse_mode='MarkdownV2')
+def send_help_text(chat_id):
+	bot.sendMessage(chat_id, HELP_TXT, parse_mode=MSG_PARSE_MODE)
+
+
+def send_text_to_admins(text):
+	for admin_chat_id in admin_chat_ids:
+		bot.sendMessage(admin_chat_id, text)
+
+
+def inform_admins(received_msg):
+	if 'username' in received_msg['from']:
+		user = f'"{received_msg["from"]["username"]}"'
+	else:
+		user = 'someone'
+	if 'text' in received_msg:
+		text = f'"{received_msg["text"]}"'
+	else:
+		text = 'something'
+	send_text_to_admins(f'{user} sent command {text}')
 
 
 def is_isoformat_time(time_str):
@@ -147,102 +170,106 @@ def is_isoformat_time(time_str):
 
 def telegram_message_handler(msg):
 	global allowed_telegram_usernames
+	global admin_chat_ids
 
 	message = msg['text'].lower().strip() if 'text' in msg else ''
 	split_msg = message.split()
 	print(f'Msg received: {msg}')
 
-	if ('username' in msg['from']
-				and msg['from']['username'] in allowed_telegram_usernames):
-		# Save chat ID; will be used for sending subsequent messages
-		global allowed_chat_id
-		allowed_chat_id = msg['chat']['id']
+	if ('username' in msg['from'] and msg['from']['username'] in
+	        (allowed_telegram_usernames + admins)):
+		chat_id = msg['chat']['id']
+
+		if msg['from']['username'] in admins:
+			admin_chat_ids |= {chat_id}
+		else:
+			inform_admins(msg)
 
 		if is_isoformat_time(message):
 			modify_settings(open_at=message)
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		# Process <h:mm> just like <hh:mm> time format, e.g. '8:32' as '08:32'
 		elif is_isoformat_time('0' + message):
 			modify_settings(open_at='0' + message)
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif message in CMDS_OFF:
 			modify_settings(open_at='off')
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif message in CMDS_DAWN:
 			modify_settings(dawn_close=True)
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif message in CMDS_NODAWN:
 			modify_settings(dawn_close=False)
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif message in CMDS_UP:
 			shutterControl.actuate_shutters(True)
-			bot.sendMessage(allowed_chat_id, _('OK, opening shutters...'))
+			sendMsg(chat_id, _('OK, opening shutters\\.\\.\\.'))
 
 		elif message in CMDS_DOWN:
 			shutterControl.actuate_shutters(False)
-			bot.sendMessage(allowed_chat_id, _('OK, closing shutters...'))
+			sendMsg(chat_id, _('OK, closing shutters\\.\\.\\.'))
 
 		elif message in CMDS_DEPRESSION_CIVIL:
 			modify_settings(depression=astral.Depression.CIVIL)
-			send_status_msg()
+			send_status_msg(chat_id)
 			
 		elif message in CMDS_DEPRESSION_NAUT:
 			modify_settings(depression=astral.Depression.NAUTICAL)
-			send_status_msg()
+			send_status_msg(chat_id)
 			
 		elif message in CMDS_DEPRESSION_ASTRO:
 			modify_settings(depression=astral.Depression.ASTRONOMICAL)
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif split_msg[0] in CMDS_DEPRESSION_FLOAT_ARG:
 			try:
 				modify_settings(depression=float(split_msg[1]))
-				send_status_msg()
+				send_status_msg(chat_id)
 			except:
-				bot.sendMessage(allowed_chat_id, _('Wrong argument format'))
-				send_help_text()
+				bot.sendMessage(chat_id, _('Wrong argument format'))
+				send_help_text(chat_id)
 
 		elif split_msg[0] in CMDS_LATEST:
 			try:
 				if split_msg[1] in CMDS_OFF:
 					modify_settings(latest='off')
-					send_status_msg()
+					send_status_msg(chat_id)
 				elif is_isoformat_time(split_msg[1]):
 					modify_settings(latest=split_msg[1])
-					send_status_msg()
+					send_status_msg(chat_id)
 				elif is_isoformat_time('0' + split_msg[1]):
 					modify_settings(latest='0'+split_msg[1])
-					send_status_msg()
+					send_status_msg(chat_id)
 				else:
-					bot.sendMessage(allowed_chat_id, _('Unknown command'))
-					send_help_text()
+					bot.sendMessage(chat_id, _('Unknown command'))
+					send_help_text(chat_id)
 			except:
-				bot.sendMessage(allowed_chat_id, _('Wrong argument format'))
-				send_help_text()
+				bot.sendMessage(chat_id, _('Wrong argument format'))
+				send_help_text(chat_id)
 
 		elif message in CMDS_STATUS:
-			send_status_msg()
+			send_status_msg(chat_id)
 
 		elif message in CMDS_HELP:
-			send_help_text()
+			send_help_text(chat_id)
 
 		else:
-			bot.sendMessage(allowed_chat_id, _('Unknown command'))
-			send_help_text()
+			bot.sendMessage(chat_id, _('Unknown command'))
+			send_help_text(chat_id)
 
 	else:
 		# This is a message from a not allowed user; react accordingly
-		bot.sendMessage(allowed_chat_id, _('Contact trial from:'))
-		bot.sendMessage(allowed_chat_id, repr(msg))
+		send_text_to_admins(_('Contact trial from:'))
+		send_text_to_admins(repr(msg))
 
 
 def shutter_control_loop():
-	global next_event, allowed_chat_id
+	global next_event
 	while True:
 		if next_event != None and next_event.time != None:
 			now = datetime.datetime.now(datetime.timezone.utc)
@@ -250,23 +277,20 @@ def shutter_control_loop():
 				current_event_open: bool = next_event.open
 				shutterControl.actuate_shutters(current_event_open)
 				update_next_event()
-				if allowed_chat_id >= 0:
-					try:
-						if current_event_open:
-							bot.sendMessage(allowed_chat_id,
-								_('Opening shutters...'))
-						else:
-							bot.sendMessage(allowed_chat_id,
-								_('Closing shutters...'))
-						send_status_msg()
-					except:
-						# If Telegram msg cannot be sent: Continue loop anyway
-						pass
+				try:
+					if current_event_open:
+						send_text_to_admins(_('Opening shutters...'))
+					else:
+						send_text_to_admins(_('Closing shutters...'))
+				except:
+					# If Telegram msg cannot be sent: Continue loop anyway
+					pass
 		time.sleep(UPDATE_EVERY_SECONDS)
 
 
 def main():
 	global bot
+	global admins
 	global allowed_telegram_usernames
 
 	# Read configuration from .ini file
@@ -277,6 +301,9 @@ def main():
 	bot_token = (
 		config[configparser.DEFAULTSECT][INI_KEY_TELEGRAM_BOT_TOKEN]
 		.strip())
+	admins = (
+		config[configparser.DEFAULTSECT][INI_KEY_ADMIN_USERS]
+		.strip().split('\n'))
 	allowed_telegram_usernames = (
 		config[configparser.DEFAULTSECT][INI_KEY_ALLOWED_TELEGRAM_USERS]
 		.strip().split('\n'))
